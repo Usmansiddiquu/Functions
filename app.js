40989fd6b34e4f514c5d7b27f131bff80cb0587a
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-05-28 14:51:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+//// question1 
+// var input = prompt("Enter a positive integer")
+// console.log(input)
+// console.log(Math.round(input))
+// console.log(Math.floor(input))
+// console.log(Math.ceil(input))
+
+//// question2
+
+// var input = prompt("Enter a negative integer")
+// console.log(input)
+// console.log(Math.round(input))
+// console.log(Math.floor(input))
+// console.log(Math.ceil(input))
+
+//// question3
+
+// var number = prompt("Enter any number")
+// function displayAbsoluteValue(number) {
+//     var absoluteValue = Math.abs(number);
+//     console.log("The absolute value of " + number + " is " + absoluteValue);
+//   }
+//   displayAbsoluteValue(number)
+  
+//  // question4
+
+// var randomNumber = Math.ceil(Math.random()*6)
+// console.log("random dice value is: "+randomNumber)
+
+ // // question5
+
+// var cointoss = Math.ceil(Math.random()*2)
+// console.log(cointoss)
+// if(cointoss == 2){
+// console.log("random coin value: Heads")
+// }
+// else{
+//   console.log("random coin value: Tails")
+// }
